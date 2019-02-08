e786d3221d48de910af1bd622890aae5e69f73f3
Ozgur Ozturk <Oozturk@evestment.com>
2019-02-08 17:31:21 -0500

    refactor

--- json_df_conversion/examples.py
+++ json_df_conversion/examples.py
@@ -38,15 +38,48 @@ df2 = pd.read_json(json_formatted_data, orient='index')
 assert df.equals(df2),  "convert and convert back failed for index oriented conversion"
 
 # =====================================
+# === columns oriented
+
+json_formatted_data = df.to_json(orient='columns')
+print("Columns oriented, Formatted as JSON:")
+print(json_formatted_data)
+# {"col 1":{"row 1":"a","row 2":"c"},"col 2":{"row 1":"b","row 2":"d"}}
+
+df2 = pd.read_json(json_formatted_data, orient='columns')
+assert df.equals(df2),  "convert and convert back failed for columns oriented conversion"
+
+# =====================================
+# === values oriented
+
+json_formatted_data = df.to_json(orient='values')
+print("values oriented, Formatted as JSON:")
+print(json_formatted_data)
+# [["a","b"],["c","d"]]
+
+df2 = pd.read_json(json_formatted_data, orient='values')
+print("Converted back from values oriented conversion")
+print(str(df2))
+#    0  1
+# 0  a  b
+# 1  c  d
+# convert and convert back for records oriented conversion won't give original result,
+# since the index and column labels got lost!
+
+# =====================================
 # === records oriented (index labels lost)
 
 json_formatted_data = df.to_json(orient='records')
-print("Index oriented, Formatted as JSON:")
+print("records oriented, Formatted as JSON:")
 print(json_formatted_data)
 # '[{"col 1":"a","col 2":"b"},
 #   {"col 1":"c","col 2":"d"}]'
 
 df2 = pd.read_json(json_formatted_data, orient='records')
+print("Converted back from records oriented conversion")
+print(str(df2))
+#   col 1 col 2
+# 0     a     b
+# 1     c     d
 # convert and convert back for records oriented conversion won't give original result,
 # since the index labels got lost!
 
